ec21ae783550bbb288aec46983c1c145d5932a80
liviazgithu <zucca.livia@icloud.com>
2026-06-11 14:52:25 +0200

    Mise à jour portfolio

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+echo "dgtfgtdtdtg";
+//$host = 'localhost';
+//$db = 'monportfolio';
+//$user = 'root';
+//$password = 'root';
+
+//$dsn = "mysql:host=$host;dbname=$db";
+//echo "dgtfgtdtdtg";
+//try {
+    //$pdo = new PDO($dsn, $user, $password);
+
+    //if ($pdo) {
+        //echo "Connection successfull";
+        //return $pdo;
+    }
+}
+//catch (PDOException $e) {
+    //echo $e->getMessage();
+
+}
+
+?>
